5ea8b7c2a4942329bc63e95ab0770dd6f69df3e8
Shruthika-Ravi <116086914+Shruthika-Ravi@users.noreply.github.com>
2022-11-07 11:43:56 +0530

    Create gitTag.groovy

---
+++ vars/gitTag.groovy
@@ -0,0 +1,14 @@
+if (fileExists()) {
+  def readcounter = readFile(file: 'version.txt')
+  readcounter = readcounter.toInteger() +1
+  def version= "Version" + readcounter
+  println(version)
+  sh 'mvn package -Dartifactversion=' + "${version}"
+  writeFile(file: 'version.txt', text:readcounter.toString())
+} 
+
+else {
+currentBuild.result = "FAILURE" 
+  } 
+
+  echo "Build and Package Completed" 
